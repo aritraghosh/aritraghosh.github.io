8552202397077235d8c82656d4f453451e46174e
Aritra Ghosh <aritrag94@gmail.com>
2025-02-15 12:38:13 +0530

    more changes

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme === 'dark') {
+        document.body.classList.add('dark-mode');
+    }
+
     toggleButton.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
+        let theme = 'light';
+        if (document.body.classList.contains('dark-mode')) {
+            theme = 'dark';
+        }
+        localStorage.setItem('theme', theme);
     });
 });
